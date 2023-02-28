f01198f90e8cac81851eeeb74396c898213f4e05
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-28 15:05:55 -0500

    omg

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,1 +1,18 @@
+items = {}
+items = {
+    1: {"Grilled Cheese": 10.00},
+    2: {"Hotdog" : 5.00},
+    3: {"Sushi" : 7.00},
+    4: {"Butter Chicken" : 8.00},
+    5: {"Greek Salad" : 6.00},
+    6: {"Poutine" : 5.00},
+}
+#print(items[2]['Hotdog'])
+cOrderSel = {}
+cOrderSel = input().split(",")    
+val = ''    
+for x in cOrderSel:
+    val = dict.fromkeys(items,x)
 
+
+print(val)
