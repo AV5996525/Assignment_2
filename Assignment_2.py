038a0e2706cd832649d7556d9c3e85f113f9c7e6
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-03-02 00:51:07 -0500

    excellent

--- Assignment_2.py
+++ Assignment_2.py
@@ -2,14 +2,17 @@
 
 #print(final)
 
-#print(items[2]['Hot dog'])
+
 
 #def gc () : 
     #cOrderTotal["Name"].append("Grilled Cheese")
     #cOrderTotal["Quantity"].append(1)
     #print(cOrderTotal)
     #return cOrderTotal
-
+from tabulate import tabulate
+def receipt (M,N,O,P,Q,R):
+    print(tabulate([ [M],[N],[O],[P],[Q],[R] ], headers = ["Order", "Item Amnt", "Item Price","Total"]))
+    
 def taxTime(L):
     hst = .13
     tax = (L) * hst
@@ -35,6 +38,7 @@ def delivy (x) :
             tip = float(input("Tip the delivery person 10, 15, or 20%"))
             tipC(tip,subT)
             taxTime(grand)
+            receipt(cOrderTotal1,cOrderTotal2,cOrderTotal3,cOrderTotal4,cOrderTotal5,cOrderTotal6)
         elif (subT) >= 100 and 500 > (subT) :
             discnt10 = (.10*(subT))
             grand = ((subT) - discnt10 -5)
@@ -42,6 +46,7 @@ def delivy (x) :
             tip = float(input("Tip the delivery person 10, 15, or 20%"))
             tipC(tip,subT)
             taxTime(grand)
+            receipt(cOrderTotal1,cOrderTotal2,cOrderTotal3,cOrderTotal4,cOrderTotal5,cOrderTotal6)
         elif (subT) < 500 :
             if subT > 30:
                 discnt5 = (.05*(subT))
@@ -50,6 +55,7 @@ def delivy (x) :
                 tip = float(input("Tip the delivery person 10, 15, or 20%"))
                 tipC(tip,subT)
                 taxTime(grand)
+                receipt(cOrderTotal1,cOrderTotal2,cOrderTotal3,cOrderTotal4,cOrderTotal5,cOrderTotal6)
             if subT < 30:
                 discnt5 = (.05*(subT))
                 grand = ((subT) - discnt5 + 5)
@@ -57,6 +63,7 @@ def delivy (x) :
                 tip = float(input("Tip the delivery person 10, 15, or 20%"))
                 tipC(tip,subT)
                 taxTime(grand)
+                receipt(cOrderTotal1,cOrderTotal2,cOrderTotal3,cOrderTotal4,cOrderTotal5,cOrderTotal6)
     if deliv == "N" :
         subT = (x) 
         if (subT) > 500:
@@ -96,47 +103,78 @@ items = {
     5: {"Greek salad" : 16.00},
     6: {"Poutine" : 12.00},
 }
-
+#print(items[2]['Hot dog'])
 cOrderTotal1 = {"Name":[],"Quantity":[]}
 cOrderTotal2 = {"Name":[],"Quantity":[]}
 cOrderTotal3 = {"Name":[],"Quantity":[]}
 cOrderTotal4 = {"Name":[],"Quantity":[]}
 cOrderTotal5 = {"Name":[],"Quantity":[]}
 cOrderTotal6 = {"Name":[],"Quantity":[]}
+cOrderTotalX = []
 
 def mm ():
+    new_list = []
+    old = []
     cOrderSel = str(input("Welcome friends"))    
     if cOrderSel == '1' : 
         cOrderTotal1["Name"].append("Grilled cheese")
         a = int(input("How many"))
+        
         cOrderTotal1["Quantity"].append(a)
+        if a > 0:
+            #old = "Grilled cheese"
+            cOrderTotalX.append("Grilled cheese")
     if cOrderSel == '2' : 
         cOrderTotal2["Name"].append("Hot dog")
         b = int(input("How many"))
         cOrderTotal2["Quantity"].append(b)
         print(str(cOrderTotal2))
+        if b > 0:
+            #old = "Hot dog"
+            cOrderTotalX.append("Hot dog")
     if cOrderSel == '3' : 
         cOrderTotal3["Name"].append("Sushi")
         c = int(input("How many"))
         cOrderTotal3["Quantity"].append(c)
+        if c > 0:
+            #old = "Sushi"
+            cOrderTotalX.append("Sushi")
     if cOrderSel == '4' : 
         cOrderTotal4["Name"].append("Butter chicken")
         d = int(input("How many"))
         cOrderTotal4["Quantity"].append(d)
+        if d > 0:
+            #old = "Butter chicken"
+            cOrderTotalX.append("Butter chicken")
     if cOrderSel == '5' : 
         cOrderTotal5["Name"].append("Greek salad")
         e = int(input("How many"))
         cOrderTotal5["Quantity"].append(e)
+        if e > 0:
+            #old = "Greek salad"
+            cOrderTotalX.append("Greek salad")
     if cOrderSel == '6' :
         cOrderTotal6["Name"].append("Poutine")
         f = int(input("How many"))
         cOrderTotal6["Quantity"].append(f)
+        if f > 0:
+            #old = "Poutine"
+            cOrderTotalX.append("Poutine")
     if cOrderSel == "C" :
+        #finalString = (sum(cOrderTotal1["Name"]))  , (sum(cOrderTotal2["Name"])) ,(sum(cOrderTotal3["Name"]))  ,(sum(cOrderTotal4["Name"]))   ,(sum(cOrderTotal5["Name"])), (sum(cOrderTotal6["Name"]))
+       
+        new_list = list(set(cOrderTotalX)) #removing duplicate by converting to set then list
+
+        print(new_list)
+
         confirm1 = input("Are you sure you want to check out")
+        
         if confirm1 == "Y":
             subT = 0
             final = (((sum(cOrderTotal1["Quantity"])) * (items[1]['Grilled cheese'])) + ((sum(cOrderTotal2["Quantity"])) * (items[2]['Hot dog'])) + ((sum(cOrderTotal3["Quantity"])) * (items[3]['Sushi'])) + ((sum(cOrderTotal4["Quantity"])) * (items[4]['Butter chicken'])) + ((sum(cOrderTotal5["Quantity"])) * (items[5]['Greek salad'])) + ((sum(cOrderTotal6["Quantity"])) * (items[6]['Poutine'])))
-            delivy(x = final)         
+            delivy(x = final)   
+            
+            
         elif confirm1 == "N":            
             mm() 
     return cOrderTotal1, cOrderTotal2, cOrderTotal3, cOrderTotal4, cOrderTotal5, cOrderTotal6
